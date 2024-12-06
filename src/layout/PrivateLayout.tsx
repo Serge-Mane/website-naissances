222e4b683d7dbe8e1a0ff0e9443cb0a530107a28
Serge-Mane <sergemane81@gmail.com>
2024-12-06 11:42:08 +0000

    creation de l'entête dans le layout en haut du contenu à droite du menu

--- src/layout/PrivateLayout.tsx
+++ src/layout/PrivateLayout.tsx
@@ -6,6 +6,10 @@ function PrivateLayout() {
         <section className="border-4 border-blue-700 min-h-screen">
             <Nav />
             <main className="wrapper pl-64 pr-10">
+                <header className="flex justify-between my-3 text-2xl font-bold">
+                    <h3>Déclarations</h3>
+                    <span>SAM</span>
+                </header>
                 <Outlet />
             </main>
         </section>
