ee74570616ac488ced80219b2b49a59b8b9f0332
Serge-Mane <sergemane81@gmail.com>
2024-12-07 11:47:47 +0000

    la date est formateé dans le fichier date.ts puis appellé dans le composant declarations.tsx pour la date

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,9 +1,14 @@
+const monthOfYears = ["Jan.", "Fev.", "Mar.", "Avr.", "Mai.", "Jui.", "Juil.", "Aou.", "Sept.", "Oct.", "Nov.", "Dec."]
+
+const formatDay = (day: number) => {
+    return `0${day}`.slice(-2)
+}
 /**
  * c'est pour formater le date 
  */
 const formatDate = (value: string) => {
     const date = new Date(value.split(" ")[0]);
-    return `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`
+    return `${formatDay(date.getDate())} ${monthOfYears[date.getMonth()]} ${date.getFullYear()}`
 
 }
 export { formatDate };
