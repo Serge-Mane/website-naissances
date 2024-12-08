e23cb76855f81bbacf90b45f67fdc207cacd91b1
Serge-Mane <sergemane81@gmail.com>
2024-12-08 08:55:33 +0000

    on a scinder aussi le hook pour creer a part la partie requete et reponse dans le fichier index.ts du services pour etre appeller dns le hook

--- src/hook/use-declarations/useDeclarations.ts
+++ src/hook/use-declarations/useDeclarations.ts
@@ -1,17 +1,17 @@
+import { search } from "@/services";
 import { Declaration } from "@/types/Declaration";
 import { useEffect, useState } from "react";
 
 function useDeclarations() {
     const [declarations, setDeclarations] = useState<Declaration[]>([]);
-    const search = async () => {
-        const response = await fetch("http://localhost:8080/declarations");
-        const data = await response.json();
+    const getDeclarations = async () => {
+        const data = await search("declarations");
         setDeclarations(data);
 
     };
 
     useEffect(() => {
-        search();
+        getDeclarations();
     }, []);
     return declarations;
 }
